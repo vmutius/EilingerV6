f11ed650ef1d34a28251bc1dad52e385ab029b7f
Bettina von Mutius <vmutius@gmail.com>
2023-01-22 17:48:47 +0100

    Wirkliche Anpassungen an DB

---
+++ app/Models/Login.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Login extends Model
+{
+    use HasFactory;
+
+    //https://laracasts.com/series/eloquent-performance-patterns/episodes/4
+}
