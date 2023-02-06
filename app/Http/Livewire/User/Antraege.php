67911d6dfdb62967c60967c0e97d8b8923028d45
Bettina von Mutius <vmutius@gmail.com>
2023-02-06 23:36:17 +0100

    Mehrere Anträge gleichzeitig

---
+++ app/Http/Livewire/User/Antraege.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+use App\Models\Application;
+
+class Antraege extends Component
+{
+    public $showModal = false;
+    public $name;
+    public $bereich;
+
+    protected $rules = [
+        'name' => 'required',
+        'bereich' => 'required',
+    ];
+
+    public function render()
+    {
+        $applications = Application::where ('user_id', auth()->user()->id)->get();
+        return view('livewire.user.antraege', [
+            'applications' => $applications
+        ])
+            ->layout(\App\View\Components\Layouts\UserDashboard::class);
+    }
+
+    public function addApplication() 
+    {
+        $this->showModal = true; 
+    }
+
+    public function deleteApplication($id)
+    {
+        
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $application= Application::create([
+            'name'=>$this->name,
+            'bereich'=>$this->bereich,
+            'user_id'=>auth()->user()->id,
+        ]);
+        
+        $application->users()->attach(auth()->user()->id); 
+        $this->showModal = false;
+    }
+
+    public function close()
+    {
+        $this->showModal = false;
+    }
+}
