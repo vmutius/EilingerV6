9aa65444e5c1bf579e58e9214a8c77d47f24c326
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-05 22:18:58 +0300

    rename country field to country_id and add foreignId in address migration

--- database/migrations/2022_05_08_104204_create_addresses_table.php
+++ database/migrations/2022_05_08_104204_create_addresses_table.php
@@ -16,11 +16,11 @@ class CreateAddressesTable extends Migration
         Schema::create('addresses', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->foreignId('user_id')->constrained();
+            $table->foreignId('country_id')->constrained();
             $table->string('street', 50);
             $table->string('number', 10)->nullable();
             $table->string('plz', 20);
             $table->string('town', 50);
-            $table->string('country', 2)->default('CH');
             $table->date('since')->nullable();
             $table->boolean('isWochenaufenthalt')->default(false);
             $table->timestamps();
