8b3d89dc19fda7ff0eae8d92a7877fbc13049fbd
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-05 21:35:04 +0300

    remove unnecessary controller

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,15 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserDashController;
 use App\Http\Controllers\AdminDashController;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
 
 
 /*
