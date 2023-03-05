436423ecc1bab7bc3bc307fb8c6ba1c526c31865
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-05 22:18:33 +0300

    remove unused imports

--- database/migrations/2022_05_08_104204_create_addresses_table.php
+++ database/migrations/2022_05_08_104204_create_addresses_table.php
@@ -3,8 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\User;
-use App\Models\Application;
 
 class CreateAddressesTable extends Migration
 {
